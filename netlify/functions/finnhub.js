d929a8714be5b3c1dc7aa01abba5def40079c62d
Nicola Bottioni <battersea.dynamics@gmail.com>
2026-04-20 23:56:37 +0100

    second version real data with search bar onall finnhub of dashboard

--- netlify/functions/finnhub.js
+++ netlify/functions/finnhub.js
@@ -3,8 +3,9 @@ const https = require('https');
 exports.handler = async function(event) {
   const symbol = event.queryStringParameters.symbol;
   const type = event.queryStringParameters.type || 'quote';
+  const query = event.queryStringParameters.query;
   const apiKey = process.env.FINNHUB_API_KEY;
-  
+
   let path;
   if(type === 'quote') {
     path = `/api/v1/quote?symbol=${symbol}&token=${apiKey}`;
@@ -12,6 +13,10 @@ exports.handler = async function(event) {
     path = `/api/v1/company-news?symbol=${symbol}&from=2026-04-13&to=2026-04-20&token=${apiKey}`;
   } else if(type === 'calendar') {
     path = `/api/v1/calendar/earnings?from=2026-04-20&to=2026-05-20&token=${apiKey}`;
+  } else if(type === 'search') {
+    path = `/api/v1/search?q=${encodeURIComponent(query)}&token=${apiKey}`;
+  } else if(type === 'profile') {
+    path = `/api/v1/stock/profile2?symbol=${symbol}&token=${apiKey}`;
   }
 
   return new Promise((resolve) => {
