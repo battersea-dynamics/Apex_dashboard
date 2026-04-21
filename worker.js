e91ac4b442abdf7edd2e415dd8dab2829f64b6dc
Nicola Bottioni <battersea.dynamics@gmail.com>
2026-04-21 23:46:56 +0100

    cloudflare version of dashboard

--- worker.js
+++ worker.js
@@ -1,13 +1,14 @@
 export default {
   async fetch(request, env) {
     const url = new URL(request.url);
-    const type = url.searchParams.get('type');
-
-    // Temporary debug — shows if key is being received
+    
     return new Response(JSON.stringify({
       hasKey: !!env.FINNHUB_API_KEY,
       keyLength: env.FINNHUB_API_KEY ? env.FINNHUB_API_KEY.length : 0,
-      type: type
+      fullUrl: request.url,
+      searchParams: Object.fromEntries(url.searchParams),
+      type: url.searchParams.get('type'),
+      symbol: url.searchParams.get('symbol')
     }), {
       headers: { 'Content-Type': 'application/json' }
     });
